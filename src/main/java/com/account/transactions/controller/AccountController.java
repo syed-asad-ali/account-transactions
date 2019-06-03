908b273aa665a2561d51e86dd687d84216e6414b
syed-asad-ali <syed_asad_ali@icloud.com>
2019-06-04 03:30:16 +1000

    Calculate opening balance from debit and credit

--- src/main/java/com/account/transactions/controller/AccountController.java
+++ src/main/java/com/account/transactions/controller/AccountController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.math.BigDecimal;
 import java.util.Optional;
 
 @RestController
@@ -36,15 +37,26 @@ public class AccountController {
      */
     @GetMapping("/accounts")
     public Page<Account> getAllAccounts(Pageable pageable) {
-        return accountRepository.findAll(pageable).map(account -> Account.builder()
-                .id(account.getId())
-                .accountName(account.getAccountName())
-                .accountNumber(account.getAccountNumber())
-                .accountType(account.getAccountType())
-                .balanceDate(account.getBalanceDate())
-                .currencyType(account.getCurrencyType())
-                .openingBalance(account.getOpeningBalance())
-                .build());
+        return accountRepository.findAll(pageable).map(account -> {
+            final Account accountBuilder = Account.builder()
+                    .id(account.getId())
+                    .accountName(account.getAccountName())
+                    .accountNumber(account.getAccountNumber())
+                    .accountType(account.getAccountType())
+                    .balanceDate(account.getBalanceDate())
+                    .currencyType(account.getCurrencyType())
+                    .openingBalance(BigDecimal.ZERO)
+                    .build();
+
+            // calculate opening balance
+            if (!account.getAccountTransactions().isEmpty()) {
+                account.getAccountTransactions().forEach(accountTransaction -> {
+                    accountBuilder.setOpeningBalance(accountBuilder.getOpeningBalance().add(accountTransaction.getCreditAmount().multiply(BigDecimal.ONE.negate()).add(accountTransaction.getDebitAmount())));
+                });
+            }
+
+            return accountBuilder;
+        });
     }
 
     /**
@@ -64,13 +76,13 @@ public class AccountController {
     /**
      * POST method added to facilitate adding AccountTransaction objects to an Account. For TESTING ONLY
      *
-     * @param accountId accountId for the Account object against which the AccountTransaction needs to be saved.
+     * @param accountId          accountId for the Account object against which the AccountTransaction needs to be saved.
      * @param accountTransaction AccountTransaction object to save
      * @return persisted Object
      */
     @PostMapping("/accounts/{accountId}/transactions")
-    public AccountTransaction createAccountTransaction(@PathVariable (value = "accountId") Long accountId,
-                                            @Valid @RequestBody AccountTransaction accountTransaction) {
+    public AccountTransaction createAccountTransaction(@PathVariable(value = "accountId") Long accountId,
+                                                       @Valid @RequestBody AccountTransaction accountTransaction) {
         return accountRepository.findById(accountId).map(account -> {
             accountTransaction.setAccount(account);
             return accountTransactionRepository.save(accountTransaction);
